789fc464debe6cd9ee7242943b4709995a229f71
bracha rubin <br667212@gmail.com>
2025-06-12 21:27:38 +0300

    change cookieOption

--- TinyUrlServer/controllers/authController.js
+++ TinyUrlServer/controllers/authController.js
@@ -24,12 +24,12 @@ const signToken = (user) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user);  
 
-  const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
+const cookieOptions = {
+  expires: new Date(
+    Date.now() + Number(process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 60 * 1000
+  ),
+  httpOnly: true,
+};
 
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
